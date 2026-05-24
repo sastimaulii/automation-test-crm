45b9fab31fb0fb794d31907aa349c821a5abf48b
sastimaulii <sastimaulii17@gmail.com>
2026-05-24 15:07:34 +0700

    update workflow

--- src/test/java/utils/DriverManager.java
+++ src/test/java/utils/DriverManager.java
@@ -7,10 +7,20 @@ public class DriverManager {
 
     private static WebDriver driver;
 
-    public static WebDriver getDriver() {
+    public static void initDriver() {
         if (driver == null) {
             driver = new ChromeDriver();
         }
+    }
+
+    public static WebDriver getDriver() {
         return driver;
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null; // 🔥 penting!
+        }
     }
 }
---
+++ src/test/java/web/steps/Hooks.java
@@ -0,0 +1,18 @@
+package web.steps;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import utils.DriverManager;
+
+public class Hooks {
+
+    @Before
+    public void setUp() {
+        DriverManager.initDriver();
+    }
+
+    @After
+    public void tearDown() {
+        DriverManager.quitDriver();
+    }
+}
